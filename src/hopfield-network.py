9c8be09c061cfb01b203f2c40ee2edb6e1d0213f
Michał Respondek <michalrespondek01@gmail.com>
2024-04-23 08:07:21 +0200

    Hopfield network

--- src/hopfield-network.py
+++ src/hopfield-network.py
@@ -7,7 +7,7 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def is_symetric(matrix):
+def is_symmetric(matrix):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
             if matrix[i][j] != matrix[j][i]:
@@ -28,10 +28,17 @@ def is_positively_defined(matrix):
     return True
 
 
+def array_contains(array_list, array):
+    for a in range(len(array_list)):
+        if np.array_equal(array_list[a], array):
+            return True
+
+    return False
+
 def ascertain_stability(matrix):
     print("Hopfield network stability")
     print("First condition: weight matrix is symmetric")
-    print("PASSED") if is_symetric(matrix) else print("FAILED")
+    print("PASSED") if is_symmetric(matrix) else print("FAILED")
     print("Second condition: weight matrix diagonal is zeroes")
     print("PASSED") if is_diagonal_zeroes(matrix) else print("FAILED")
 
@@ -39,7 +46,7 @@ def ascertain_stability(matrix):
         print("Third condition: weight matrix has positive definiteness")
         print("PASSED") if is_positively_defined(matrix) else print("FAILED")
 
-    if sync == False and is_symetric(matrix) and is_diagonal_zeroes(matrix) or sync == True and is_symetric(
+    if sync == False and is_symmetric(matrix) and is_diagonal_zeroes(matrix) or sync == True and is_symmetric(
             matrix) and is_diagonal_zeroes(matrix) and is_positively_defined(matrix):
         print("The network will certainly stabilize")
     else:
@@ -60,7 +67,7 @@ def synchronous_mode(x, w, f, i):
 
 
 X = np.random.rand(3, 1)
-X_prime = []
+X_prev = []
 
 print(X)
 
@@ -76,16 +83,24 @@ ascertain_stability(W)
 
 for i in range(10000):
     print(i)
-    X_prime = X.copy()
+    X_prev.append(X.copy())
+
+    if len(X_prev) > 8:
+        X_prev = X_prev[1:]
+
     X = synchronous_mode(X, W, sigmoid, 2)
 
-    if np.array_equal(X, X_prime):
+    if array_contains(X_prev, X):
         break
 
-if np.array_equal(X, X_prime):
+if np.array_equal(X, X_prev[len(X_prev) - 1]):
     print("Network is stable")
     print(X)
 else:
     print("Network failed to stabilize")
+    for i in range(len(X_prev)):
+        if np.array_equal(X_prev[len(X_prev) - i - 1], X):
+            print("Cycle every " + str(i+1) + " iterations")
+
     print(X)
-    print(X_prime)
+    print(X_prev)
