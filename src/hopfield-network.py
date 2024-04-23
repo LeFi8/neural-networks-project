8ae113017ca54e01c7e374a0a46f319e31657652
Michał Respondek <michalrespondek01@gmail.com>
2024-04-23 10:51:01 +0200

    Hopfield network

--- src/hopfield-network.py
+++ src/hopfield-network.py
@@ -35,6 +35,7 @@ def array_contains(array_list, array):
 
     return False
 
+
 def ascertain_stability(matrix):
     print("Hopfield network stability")
     print("First condition: weight matrix is symmetric")
@@ -42,15 +43,23 @@ def ascertain_stability(matrix):
     print("Second condition: weight matrix diagonal is zeroes")
     print("PASSED") if is_diagonal_zeroes(matrix) else print("FAILED")
 
-    if sync == True:
+    if sync is True:
         print("Third condition: weight matrix has positive definiteness")
         print("PASSED") if is_positively_defined(matrix) else print("FAILED")
 
-    if sync == False and is_symmetric(matrix) and is_diagonal_zeroes(matrix) or sync == True and is_symmetric(
+    if sync is False and is_symmetric(matrix) and is_diagonal_zeroes(matrix) or sync == True and is_symmetric(
             matrix) and is_diagonal_zeroes(matrix) and is_positively_defined(matrix):
         print("The network will certainly stabilize")
     else:
         print("It is not certain if the network will stabilize")
+
+
+def find_cycle(x, x_prev):
+    for i in range(len(x_prev)):
+        if np.array_equal(x_prev[len(x_prev) - i - 1], x):
+            return i
+
+    return -1
 
 
 def synchronous_mode(x, w, f, i):
@@ -81,8 +90,7 @@ print(W)
 
 ascertain_stability(W)
 
-for i in range(10000):
-    print(i)
+for iter in range(10000):
     X_prev.append(X.copy())
 
     if len(X_prev) > 8:
@@ -93,14 +101,16 @@ for i in range(10000):
     if array_contains(X_prev, X):
         break
 
-if np.array_equal(X, X_prev[len(X_prev) - 1]):
+cycle = find_cycle(X, X_prev)
+X_prev.append(X.copy())
+
+if cycle == 0:
     print("Network is stable")
     print(X)
-else:
+elif cycle > 0:
     print("Network failed to stabilize")
-    for i in range(len(X_prev)):
-        if np.array_equal(X_prev[len(X_prev) - i - 1], X):
-            print("Cycle every " + str(i+1) + " iterations")
-
-    print(X)
+    print("Cycle every " + str(cycle + 1) + " iterations")
+    print(X_prev)
+elif cycle == -1:
+    print("Network failed to stabilize")
     print(X_prev)
